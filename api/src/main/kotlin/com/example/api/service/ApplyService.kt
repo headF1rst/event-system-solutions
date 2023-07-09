835f12246ba7ad2b9d2567da2a27b57e32f00396
Sanha Ko <55884834+headF1rst@users.noreply.github.com>
2023-07-09 17:58:40 +0900

    feat: 레디스 사용하여 쿠폰 발급 문제 해결

--- api/src/main/kotlin/com/example/api/service/ApplyService.kt
+++ api/src/main/kotlin/com/example/api/service/ApplyService.kt
@@ -1,19 +1,23 @@
 package com.example.api.service
 
 import com.example.api.domain.Coupon
+import com.example.api.repository.CouponCountRepository
 import com.example.api.repository.CouponRepository
 import org.springframework.stereotype.Service
 
 @Service
 class ApplyService(
     val couponRepository: CouponRepository,
+    val couponCountRepository: CouponCountRepository,
 ) {
 
     fun apply(userId: Long) {
-        val count = couponRepository.count()
+        val count = couponCountRepository.increment()
 
-        if (count > 100) {
-            return
+        if (count != null) {
+            if (count > 100) {
+                return
+            }
         }
 
         couponRepository.save(Coupon(userId))
