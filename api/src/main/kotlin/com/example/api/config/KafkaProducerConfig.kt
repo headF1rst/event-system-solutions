e4249502e8584a3ec7eb471c0f8f21dbc6fd1adf
Sanha Ko <55884834+headF1rst@users.noreply.github.com>
2023-07-11 23:17:16 +0900

    feat: kafka consumer 모듈 추가

--- api/src/main/kotlin/com/example/api/config/KafkaProducerConfig.kt
+++ api/src/main/kotlin/com/example/api/config/KafkaProducerConfig.kt
@@ -6,7 +6,7 @@ import org.apache.kafka.common.serialization.StringSerializer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.core.DefaultKafkaProducerFactory
-import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.core.KafkaTemplate 
 import org.springframework.kafka.core.ProducerFactory
 
 @Configuration
